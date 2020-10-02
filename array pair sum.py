2bbd64b9409658fc2ac2ab88f3ccabb395f85321
graceolivia <gracelidinskysmith@gmail.com>
2020-10-02 11:29:29 -0400

    array pair sum

---
+++ array pair sum.py
@@ -0,0 +1,24 @@
+#given integer array, output hte unique pairs that sum up to specific value k
+# do the input:
+# pair_sum([1, 3, 2, 2], 4)
+#would return 2 pairs: (1,3) (2,2)
+
+def pair_sum(array, k):
+    if len(array) < 2:
+        print('too small')
+
+    seen = set()
+    output = set()
+
+    for num in array:
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+
+        else:
+            output.add((min(num, target), max(num, target)))
+
+    print('\n'.join(map(str, list(output))))
+
+pair_sum([1,3,2,2], 4)
